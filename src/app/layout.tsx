9f1893e4db51cbbd0912738d872e0c89182f3998
joao6861 <joaopedro6861@hotmail.com>
2025-11-16 08:21:52 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body className="bg-[#001428] text-white">
+      <body className="bg-white text-black">
         {/* NavBar removido */}
         <main>{children}</main>
       </body>
