09d32d20bbe9dc4b3d197b55db34c74158ae4cf6
joao6861 <joaopedro6861@hotmail.com>
2025-11-16 09:26:03 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,16 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Tureggon",
-  description: "Sistema Tureggon iguais ao layout da Lasy",
+  title: "Tureggon Store",
+  description: "Sistema Tureggon igual ao layout da Lasy",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className="bg-gradient-to-b from-[#02C4FF] via-[#0091FF] to-[#001F3F] min-h-screen text-white">
+      <body className="min-h-screen w-full">
         {children}
       </body>
     </html>
   );
 }
-
