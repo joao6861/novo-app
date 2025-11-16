7a2a6b419ae6e005ed7d8f64a81585c32f82f02d
joao6861 <joaopedro6861@hotmail.com>
2025-11-16 08:56:09 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className="bg-gradient-to-b from-[#009dff] via-[#0064d4] to-[#001428] text-white min-h-screen">
+      <body className="bg-gradient-to-b from-[#02C4FF] via-[#0091FF] to-[#001F3F] text-white min-h-screen">
         <NavBar />
-        <main className="pt-4">{children}</main>
+        <main className="pt-4">
+          {children}
+        </main>
       </body>
     </html>
   );
