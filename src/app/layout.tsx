f5e7804ba1b57b15372f0f07233a352acc60b273
joao6861 <joaopedro6861@hotmail.com>
2025-11-16 09:06:22 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,17 @@
 import "./globals.css";
-import NavBar from "@/components/layout/NavBar";
 
 export const metadata = {
-  title: "Tureggon Store",
-  description: "Sistema e aplicativo oficial da Tureggon Store",
+  title: "Tureggon",
+  description: "Sistema Tureggon iguais ao layout da Lasy",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className="bg-gradient-to-b from-[#02C4FF] via-[#0091FF] to-[#001F3F] text-white min-h-screen">
-        <NavBar />
-        <main className="pt-4">
-          {children}
-        </main>
+      <body className="bg-gradient-to-b from-[#02C4FF] via-[#0091FF] to-[#001F3F] min-h-screen text-white">
+        {children}
       </body>
     </html>
   );
 }
+
