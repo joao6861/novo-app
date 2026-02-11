29fbb088933b16ec0197d7f335596cfeba764525
joao6861 <joaopedro6861@hotmail.com>
2026-02-11 14:24:37 -0300

    Update route.ts

--- src/app/api/consulta-placa/route.ts
+++ src/app/api/consulta-placa/route.ts
@@ -4,19 +4,26 @@ export async function POST(req: Request) {
   try {
     const { placa } = await req.json();
 
+    if (!placa) {
+      return NextResponse.json(
+        { error: "Placa obrigatória" },
+        { status: 400 }
+      );
+    }
+
     const response = await fetch("https://apibrasil.com.br/api/consulta/placa", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 441924ef-3d2f-42ea-99c0-ed9766281347"
+        "x-api-id": "da626099-50be-47fe-a8bd-67e3f6289b48",
+        "x-api-token": "441924ef-3d2f-42ea-99c0-ed9766281347"
       },
       body: JSON.stringify({
-        placa: placa,
-        id: "da626099-50be-47fe-a8bd-67e3f6289b48"
+        placa: placa
       }),
     });
 
-    const data = await response.text();
+    const data = await response.json();
 
     return NextResponse.json({
       status_api: response.status,
@@ -25,7 +32,7 @@ export async function POST(req: Request) {
 
   } catch (error: any) {
     return NextResponse.json(
-      { erro_conexao: error.message },
+      { erro_real: error.message },
       { status: 500 }
     );
   }
